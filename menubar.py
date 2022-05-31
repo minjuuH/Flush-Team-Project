10e0f58cc65e49d74f9dac7b42d1ea09d369e4ec
Minju H <hwangmj1124@gmail.com>
2022-05-31 23:27:44 +0900

    메뉴바 수정(외부창에 연결할 메뉴바)
    외부창에서 메뉴바 기능을 사용하면 외부창이 생성되지 않고 화면이 전환되게 수정

--- menubar.py
+++ menubar.py
@@ -2,17 +2,18 @@ from tkinter import *
 import Rent_View as rv
 import Book_def as Bd
 import User_View as uv
+from UI_Class import *
 
-def MenuBar(window):
+def MenuBar(window, base_win=None):
     menubar = Menu(window)
     window.config(menu=menubar)
 
     bookmenu=Menu(menubar, tearoff=0)       #tearoff=0 -> 하위메뉴미리보기?를 없애준다
     menubar.add_cascade(label="도서관리", menu=bookmenu)
-    bookmenu.add_command(label="도서등록", command=lambda: Bd.creaNewWindow_book_r('도서 등록', window))
-    bookmenu.add_command(label="도서조회", command=lambda: Bd.createNewWindow_book_s("도서 조회", window))
-    bookmenu.add_command(label="도서수정", command=lambda: Bd.createNewWindow_book_m('도서 수정', window))
-    bookmenu.add_command(label="도서삭제", command=lambda: Bd.createNewWindow_book_del('도서 삭제', window))
+    bookmenu.add_command(label="도서등록", command=lambda: Bd.createNewWindow_book_r(base_win, 1))
+    bookmenu.add_command(label="도서조회", command=lambda: Bd.createNewWindow_book_s(base_win, 1))
+    bookmenu.add_command(label="도서수정", command=lambda: Bd.createNewWindow_book_m(base_win, 1))
+    bookmenu.add_command(label="도서삭제", command=lambda: Bd.createNewWindow_book_del(base_win, 1))
 
     usermenu=Menu(menubar, tearoff=0)
     menubar.add_cascade(label="회원관리", menu=usermenu)
@@ -23,5 +24,5 @@ def MenuBar(window):
 
     rentmenu=Menu(menubar, tearoff=0)
     menubar.add_cascade(label="대출", menu=rentmenu)
-    rentmenu.add_command(label="도서대출", command=lambda:rv.Rent_Screen(window))
-    rentmenu.add_command(label="도서반납", command=lambda:rv.Return_Screen(window))
+    rentmenu.add_command(label="도서대출", command=lambda:rv.Rent_Screen(base_win, 1))
+    rentmenu.add_command(label="도서반납", command=lambda:rv.Return_Screen(base_win, 1))
