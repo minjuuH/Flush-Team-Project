54af729d71ec3f0a8340b7e9dc34c5a51b31a23e
Minju H <hwangmj1124@gmail.com>
2022-06-06 20:11:10 +0900

    외부창 생성->창전환 방식으로 변경
    창 전환이 가능하게 하기 위해 메인 화면에 make_Frame()으로 프레임을 지정하고 해당 프레임에 위젯을 배치
    함수 매개 변수 수정에 따른 각 화면생성함수에 전달하는 인자 수정

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 import Rent_View as rv
 import Book_def as Bd
 import User_View as uv
+import UI_Class as UC
 
 #메인메뉴 창 생성
 window = Tk()
@@ -9,10 +10,14 @@ window.title("도서관리시스템")              #윈도창 제목 지정
 window.geometry("960x720")                  #화면 사이즈 지정
 window.resizable(width=False, height=False) #가로세로 크기 변경 불가
 
+#창전환을 위한 프레임 생성
+Base = UC.new_window()
+Base.make_Frame(window)
+
 #command로 전달하는 명령 함수에 인자 전달 받는 법 -> lambda: 사용
-Book = Button(window, text="도서", font=("돋움", 50), fg='white', background="gray", height=2, width=6, command=lambda:Bd.createNewWindow_book_s(window))
-User = Button(window, text="회원", font=("돋움", 50), fg='white', background="gray", height=2, width=6, command=lambda:uv.userwindow(window))
-Rent = Button(window, text="대출", font=("돋움", 50), fg='white', background="gray", height=2, width=6, command=lambda: rv.Rent_Screen(window))
+Book = Button(Base.base_frame, text="도서", font=("돋움", 50), fg='white', background="gray", height=2, width=6, command=lambda:Bd.createNewWindow_book_s(window, uc=Base))
+User = Button(Base.base_frame, text="회원", font=("돋움", 50), fg='white', background="gray", height=2, width=6, command=lambda:uv.userwindow(window, uc=Base))
+Rent = Button(Base.base_frame, text="대출", font=("돋움", 50), fg='white', background="gray", height=2, width=6, command=lambda: rv.Rent_Screen(window, uc=Base))
 
 Book.place(x=110, y=265)
 User.place(x=360, y=265)
@@ -24,21 +29,21 @@ window.config(menu=menubar)
 
 bookmenu=Menu(menubar, tearoff=0)       #tearoff=0 -> 하위메뉴미리보기?를 없애준다
 menubar.add_cascade(label="도서관리", menu=bookmenu)
-bookmenu.add_command(label="도서등록", command=lambda: Bd.createNewWindow_book_r(window))
-bookmenu.add_command(label="도서조회", command=lambda: Bd.createNewWindow_book_s(window))
-bookmenu.add_command(label="도서수정", command=lambda: Bd.createNewWindow_book_m(window))
-bookmenu.add_command(label="도서삭제", command=lambda: Bd.createNewWindow_book_del(window))
+bookmenu.add_command(label="도서등록", command=lambda: Bd.createNewWindow_book_r(window, uc=Base))
+bookmenu.add_command(label="도서조회", command=lambda: Bd.createNewWindow_book_s(window, uc=Base))
+bookmenu.add_command(label="도서수정", command=lambda: Bd.createNewWindow_book_m(window, uc=Base))
+bookmenu.add_command(label="도서삭제", command=lambda: Bd.createNewWindow_book_del(window, uc=Base))
 
 usermenu=Menu(menubar, tearoff=0)
 menubar.add_cascade(label="회원관리", menu=usermenu)
-usermenu.add_command(label="회원등록", command =lambda: uv.userwindowadd(window))
-usermenu.add_command(label="회원조회", command =lambda: uv.userwindow(window))
-usermenu.add_command(label="회원수정", command =lambda: uv.re_userwindow(window))
-usermenu.add_command(label="회원삭제", command =lambda: uv.del_userwindow(window))
+usermenu.add_command(label="회원등록", command =lambda: uv.userwindowadd(window, uc=Base))
+usermenu.add_command(label="회원조회", command =lambda: uv.userwindow(window, uc=Base))
+usermenu.add_command(label="회원수정", command =lambda: uv.re_userwindow(window, uc=Base))
+usermenu.add_command(label="회원삭제", command =lambda: uv.del_userwindow(window, uc=Base))
 
 rentmenu=Menu(menubar, tearoff=0)
 menubar.add_cascade(label="대출", menu=rentmenu)
-rentmenu.add_command(label="도서대출", command=lambda:rv.Rent_Screen(window))
-rentmenu.add_command(label="도서반납", command=lambda:rv.Return_Screen(window))
+rentmenu.add_command(label="도서대출", command=lambda:rv.Rent_Screen(window, uc=Base))
+rentmenu.add_command(label="도서반납", command=lambda:rv.Return_Screen(window, uc=Base))
 
 window.mainloop()
