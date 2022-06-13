3a25ed97f0ded90ba2be6551e2263ef4a171022c
칸 <hwangmj1124@gmail.com>
2022-06-13 22:41:18 +0900

    대출 전체 기능 구축 완료
    반납 시, 연체 여부를 파악하여 대출 가능일을 설정하는 기능 추가
    -> 연체되었을 경우, 메시지창으로 대출가능일에 대한 정보를 추가로 전달하도록 설계
    검색 시, 목록에 존재하지 않는 키워드를 검색할 경우 오류가 발생하는 부분 수정
    도서 선택 시, 도서를 선택하지 않고 확인 버튼을 눌렀을 경우 오류가 발생하는 부분 수정
    회원/도서 선택창에서 탈퇴회원을 제외/대출 중인 도서를 제외하고 목록이 출력, 제외한 목록을 대상으로 검색 기능이 실행될 수 있도록 수정

--- Rent_Dataframe.py
+++ Rent_Dataframe.py
@@ -46,8 +46,7 @@ class Rent_DF:
         
         overdue = dt.timedelta()
         for i in self.rent[self.rent['USER_PHONE']==self.re_user].index:
-            print(self.rent.loc[i,'BOOK_ISBN'])
-            if self.rent.loc[i,'BOOK_ISBN'] in self.re_book:
+            if self.rent.loc[i,'BOOK_ISBN'] in self.re_book:    #선택된 도서만 반납되도록 함
                 self.rent.loc[i, 'RETURN_DATE'] = self.re_day
                 #도서를 반납하는만큼 회원의 도서대출권수를 줄여준다.
                 self.user.loc[self.user['USER_PHONE']==self.re_user, 'USER_RENT_CNT'] -= 1
@@ -55,147 +54,23 @@ class Rent_DF:
                 #대출 정보에 문자열로 저장된 반납예정일 데이터를 date 타입으로 변경
                 self.re_due_day = dt.datetime.strptime(self.rent['RETURN_DUE_DATE'][i], '%Y-%m-%d').date()
                 #더 많이 연체된 날짜만큼 대출가능일을 늦추기 위해
-                if overdue < self.re_day-self.re_due_day:     
+                if overdue < self.re_day-self.re_due_day: 
                     overdue = self.re_day-self.re_due_day
 
         if overdue <= dt.timedelta(0):
-            print('연체된 도서가 없습니다.')
             return True
         else:
             #대출가능일 지정
-            self.user.loc[self.user['USER_PHONE']==self.re_user, 'USER_RENT_ALW']=self.re_day+overdue
+            self.alw_day = self.re_day+overdue
+            self.user.loc[self.user['USER_PHONE']==self.re_user, 'USER_RENT_ALW']=self.alw_day
             #경고횟수 추가
             self.user.loc[self.user['USER_PHONE']==self.re_user, 'USER_WARN']+=1
-            #print('연체되었습니다.')
-            #print('대출가능일 : {}'.format((re_day+overdue).strftime('%Y.%m.%d')))
             return False
 
     def Rent_replus(self, user_phone):
-        # user_phone = user['USER_PHONE'][1]
-        
-        # #선택한 회원의 대출 정보가 있는지 검사
-        # if (rent['USER_PHONE']==user_phone).any:
-        #     print(rent[rent['USER_PHONE']==user_phone])
-        # else:
-        #     print('대출 정보가 없습니다.')
-        #     quit()
-
         idx = self.rent[self.rent['USER_PHONE']==user_phone].index
         #print(idx[0])
         re_due_day = dt.datetime.strptime(self.rent.loc[idx[0], 'RETURN_DUE_DATE'], '%Y-%m-%d').date()
         self.rent.loc[idx[0], 'RETURN_DUE_DATE'] = re_due_day+dt.timedelta(days=7)  #연장할 도서 선택
         #print('연장 완료되었습니다.')
         #print('반납예정일 : {}'.format((rent.loc[idx[0], 'RETURN_DUE_DATE']).strftime('%Y.%m.%d')))
-
-
-# book = pd.read_csv('BOOK.csv', encoding = 'utf-8')
-# user = pd.read_csv('USER.csv', encoding = 'utf-8')
-
-# #초기 대출 정보 설정
-# rent_user = user['USER_PHONE'][3]
-# rent = pd.DataFrame(columns=['BOOK_ISBN', 'USER_PHONE', 'RENT_DATE', 'RETURN_DUE_DATE', 'RETURN_DATE'])
-# rent = pd.DataFrame({'BOOK_ISBN' : [book['BOOK_ISBN'][0], book['BOOK_ISBN'][1]],
-#                      'USER_PHONE' : [rent_user]*2,
-#                      'RENT_DATE' : [dt.date(2022, 5, 8),dt.date(2022, 5, 13)],
-#                      'RETURN_DUE_DATE' : [dt.date(2022, 5, 15),dt.date(2022, 5, 20)],
-#                      'RETURN_DATE': [None]*2})
-
-# user.loc[user['USER_PHONE']==rent_user, 'USER_RENT_CNT'] += len(rent)
-
-# rent.to_csv('RENT.csv', index = False, encoding = 'utf-8')
-# user.to_csv('USER.csv', index = False, encoding = 'utf-8')
-
-# #대출 정보 추가
-# def Rent_Add():
-#     book = pd.read_csv('BOOK.csv', encoding = 'utf-8')
-#     user = pd.read_csv('USER.csv', encoding = 'utf-8')
-#     rent = pd.read_csv('RENT.csv', encoding = 'utf-8')
-
-#     #한 회원이 두 권의 책을 빌리려고 하는 경우
-#     rent_book = [book['BOOK_ISBN'][2], book['BOOK_ISBN'][1]]    #대출도서의 정보는 해당 도서의 isbn으로 저장
-#     rent_user = user['USER_PHONE'][1]                           #대출회원의 정보는 해당 회원의 전화번호로 저장
-#     rent_day = [dt.datetime.now().date()]*2                     #대출 기능을 수행하는 당일을 대출일로 저장
-#     re_day = [dt.datetime.now().date()+dt.timedelta(days=7)]*2  #대출일로부터 7일 뒤를 반납예정일로 저장
-
-#     add_rent = pd.DataFrame({'BOOK_ISBN' : rent_book,
-#                          'USER_PHONE' : [rent_user]*2,
-#                          'RENT_DATE' : rent_day,
-#                          'RETURN_DUE_DATE' : re_day})
-
-#     for i in add_rent.index:
-#         if (rent['BOOK_ISBN']==add_rent['BOOK_ISBN'][i]).any():
-#             print("대출 불가능한 도서입니다.")
-#             add_rent = add_rent.drop([i])   #이미 저장된 도서는 중복 저장할 수 없으므로 목록에서 삭제
-#         else:
-#             print("대출 가능한 도서입니다.")
-
-#     rent = pd.concat([rent, add_rent], ignore_index=True)
-#     #도서를 대출하는 회원의 도서대출권수를 대출한 도서만큼 더해준다.
-#     user.loc[user['USER_PHONE']==rent_user, 'USER_RENT_CNT'] += len(add_rent)
-
-#     print(rent)
-#     rent.to_csv('RENT.csv', index = False, encoding = 'utf-8')
-#     user.to_csv('USER.csv', index = False, encoding = 'utf-8')
-    
-# #대출 정보 삭제(반납)
-# def Rent_return():
-#     user = pd.read_csv('USER.csv', encoding = 'utf-8')
-#     rent = pd.read_csv('RENT.csv', encoding = 'utf-8')
-
-#     user_phone = user['USER_PHONE'][3]
-#     #선택한 회원의 대출 정보가 있는지 검사
-#     if (rent['USER_PHONE']==user_phone).any:
-#         print(rent[rent['USER_PHONE']==user_phone])
-#     else:
-#         print('대출 정보가 없습니다.')
-#         quit()
-
-#     re_day = dt.datetime.now().date()
-    
-#     overdue = dt.timedelta()
-#     for i in rent[rent['USER_PHONE']==user_phone].index:
-#         rent.loc[i, 'RETURN_DATE'] = re_day
-#         #도서를 반납하는만큼 회원의 도서대출권수를 줄여준다.
-#         user.loc[user['USER_PHONE']==user_phone, 'USER_RENT_CNT'] -= 1
-
-#         re_due_day = dt.datetime.strptime(rent['RETURN_DUE_DATE'][i], '%Y-%m-%d').date()
-#         #더 많이 연체된 날짜만큼 대출가능일을 늦추기 위해
-#         if overdue < re_day-re_due_day:     
-#             overdue = re_day-re_due_day
-
-#     if overdue <= dt.timedelta(0):
-#         print('연체된 도서가 없습니다.')
-#     else:
-#         #대출가능일 지정
-#         user.loc[user['USER_PHONE']==user_phone, 'USER_RENT_ALW']=re_day+overdue
-#         #경고횟수 추가
-#         user.loc[user['USER_PHONE']==user_phone, 'USER_WARN']+=1
-#         print('연체되었습니다.')
-#         print('대출가능일 : {}'.format((re_day+overdue).strftime('%Y.%m.%d')))
-
-#     rent.to_csv('RENT.csv', index = False, encoding = 'utf-8')
-#     user.to_csv('USER.csv', index = False, encoding = 'utf-8')
-        
-# #대출 정보 수정(반납예정일 연장)
-# def Rent_replus():
-#     user = pd.read_csv('USER.csv', encoding = 'utf-8')
-#     rent = pd.read_csv('RENT.csv', encoding = 'utf-8')
-
-#     user_phone = user['USER_PHONE'][1]
-#     #선택한 회원의 대출 정보가 있는지 검사
-#     if (rent['USER_PHONE']==user_phone).any:
-#         print(rent[rent['USER_PHONE']==user_phone])
-#     else:
-#         print('대출 정보가 없습니다.')
-#         quit()
-
-#     idx = rent[rent['USER_PHONE']==user_phone].index
-#     print(idx[0])
-#     re_due_day = dt.datetime.strptime(rent.loc[idx[0], 'RETURN_DUE_DATE'], '%Y-%m-%d').date()
-#     rent.loc[idx[0], 'RETURN_DUE_DATE'] = re_due_day+dt.timedelta(days=7)  #연장할 도서 선택
-#     print('연장 완료되었습니다.')
-#     print('반납예정일 : {}'.format((rent.loc[idx[0], 'RETURN_DUE_DATE']).strftime('%Y.%m.%d')))
-
-#     rent.to_csv('RENT.csv', index = False, encoding = 'utf-8')
-#     user.to_csv('USER.csv', index = False, encoding = 'utf-8')
-
--- Rent_View.py
+++ Rent_View.py
@@ -7,27 +7,22 @@ import Rent_Dataframe as RD
 import pandas as pd
 import datetime as dt
 
-#실제로 사용할 함수
-# def search_info(win, choice=True):
-#     #choice->회원 정보에서 조회할 지, 도서 정보에서 조회할지 선택(True=회원, False=도서)
-#     search = win.input_text.get()   #기입창에 입력한 데이터 추출
-#     search_data = ['검색 후', search]   #search로 데이터 프레임에서 추출한 데이터 저장(추후 수정)
-#     win.info_list(text_del=1, list=search_data)
-
-#테스트용
-def search_info(win, B_DataFrame, U_DataFrame, choice=True):
+#검색 시 사용할 커맨드
+def search_info(win, B_DataFrame=None, U_DataFrame=None, choice=True):
     search = win.input_text.get()   #기입창에 입력한 데이터 추출
     data = list()
     if choice:
         search_data = User_Search(U_DataFrame, search)
-        for i in search_data.index:
-            add_list = [search_data.loc[i,'USER_NAME'],search_data.loc[i,'USER_BIRTH'],search_data.loc[i,'USER_PHONE']]
-            data.append(add_list)
+        if len(search_data)!=0:
+            for i in search_data.index:
+                add_list = [search_data.loc[i,'USER_NAME'],search_data.loc[i,'USER_BIRTH'],search_data.loc[i,'USER_PHONE']]
+                data.append(add_list)
     else:
         search_data = search_book(B_DataFrame, search)
-        for i in search_data.index:
-            add_list = [search_data.loc[i,'BOOK_TITLE'],search_data.loc[i,'BOOK_AUTHOR'],search_data.loc[i,'BOOK_PUB'],search_data.loc[i,'BOOK_ISBN']]
-            data.append(add_list)
+        if len(search_data)!=0:
+            for i in search_data.index:
+                add_list = [search_data.loc[i,'BOOK_TITLE'],search_data.loc[i,'BOOK_AUTHOR'],search_data.loc[i,'BOOK_PUB'],search_data.loc[i,'BOOK_ISBN']]
+                data.append(add_list)
 
     win.info_list(text_del=1, list=data)
 
@@ -35,7 +30,6 @@ def search_info(win, B_DataFrame, U_DataFrame, choice=True):
 def User_Search(u_DF, search):
     user = u_DF
     if(user['USER_PHONE'].str.contains(search).any()) or (user['USER_NAME'].str.contains(search).any()):
-        # print('등록되어 있지 않은 회원입니다.')
         return user.loc[user['USER_PHONE'].str.contains(search) | user['USER_NAME'].str.contains(search)]
     else:
         return []
@@ -145,7 +139,6 @@ def Return_Screen(window, uc=None):
     def re_acc(re_IF, win, new_win):
         for i in new_win.chk_list:
             re_IF.re_book.append(i[3])
-        print(re_IF.re_book)
         if re_IF.re_user==None:
             messagebox.showwarning('도서관리시스템', '회원이 선택되지 않았습니다.')
         elif len(re_IF.re_book)==0:
@@ -153,9 +146,12 @@ def Return_Screen(window, uc=None):
         else:
             msg_Ans = messagebox.askokcancel('도서관리시스템', "반납하시겠습니까?")  #부울값 반환 -> ok:True, cancel:False
             if msg_Ans:
-                re_IF.Rent_return()
-                re_IF.to_csv()
-                messagebox.showinfo('도서관리시스템', "반납 처리되었습니다.\n반납예정일:{}\n반납일:{}".format(re_IF.re_due_day, re_IF.re_day))
+                if re_IF.Rent_return():
+                    re_IF.to_csv()
+                    messagebox.showinfo('도서관리시스템', "반납 처리되었습니다.\n반납예정일:{}\n반납일:{}".format(re_IF.re_due_day, re_IF.re_day))
+                else:
+                    re_IF.to_csv()
+                    messagebox.showinfo('도서관리시스템', "연체되었습니다.\n반납예정일:{}\n반납일:{}\n대출가능일:{}".format(re_IF.re_due_day, re_IF.re_day, re_IF.alw_day.strftime('%Y.%m.%d')))
                 Return_Screen(window, new_win)
 
     new_win.Choice_base()
@@ -166,13 +162,13 @@ def Return_Screen(window, uc=None):
     #대여 목록 출력
     new_win.rent_list()
 
-#회원 선택 기능
+#회원 선택 기능 -> 확인 버튼 클릭 시 수행
 def chkU_def(win, root_win, rent, re=False):
-    if len(win.chk_list)==0:
+    if len(win.chk_list)==0:    #선택된 회원이 없을 경우
         messagebox.showwarning("회원 선택", "회원을 선택하십니오.", parent=win.newWindow)
-    elif len(win.chk_list)==1:
+    elif len(win.chk_list)==1:  #한 명의 회원만 선택되었을 경우 -> 정상 작동
         root_win.userinfo.config(text=win.chk_list[0][0]+' | '+win.chk_list[0][1]+' | '+win.chk_list[0][2])
-        if re:
+        if re:  #반납창에서 회원 선택을 수행할 경우
             rent_DF = rent.existence(win.chk_list[0][2])
             rent.re_user = win.chk_list[0][2]   #반납할 회원 정보 rent 객체에 저장
             book_list = list()
@@ -181,44 +177,56 @@ def chkU_def(win, root_win, rent, re=False):
                 idx = rent_book.index
                 book_list.append([rent_book.loc[idx[0],'BOOK_TITLE'],rent_book.loc[idx[0],'BOOK_AUTHOR'],rent_book.loc[idx[0],'BOOK_PUB'],rent_book.loc[idx[0],'BOOK_ISBN']])
             root_win.info_list(text_del=1, list=book_list)
-        else:
+        else:   #대출창에서 회원 선택을 수행할 경우
             root_win.rentU_label.config(text="대여인:"+win.chk_list[0][0])
             rent.rent_user = win.chk_list[0][2] #대출정보에 사용할 회원 전화번호 데이터 저장
             root_win.rent_day.config(text="대여일:{}".format(dt.datetime.now().date()))
             root_win.re_day.config(text="반납예정일:{}".format(dt.datetime.now().date()+dt.timedelta(days=7)))
         win.newWindow.withdraw()
-    else:
+    else:                       #선택된 회원이 한 명을 초과한 경우
         messagebox.showwarning("회원 선택", "회원 선택은 1명만 가능합니다.", parent=win.newWindow)
         
-#도서 선택 기능
+#도서 선택 기능 -> 확인 버튼 클릭 시 수행
 def chkB_def(win, root_win, rent):
-    root_win.text.config(state=NORMAL)  #텍스트 위젯을 비워주기 위해 위젯 상태를 변경가능한 상태로 설정
-    root_win.text.delete("1.0", "end")
-    plus = 0
-    for i in range(len(win.chk_list)):
-        for j in win.chk_list[i]:
-            root_win.text.insert('end', str(j)+' | ')
-        root_win.text.insert('end', '\n')
-        if i > 0:
-            plus += 1
-        rent.rent_book.append(win.chk_list[i][3])   #대출정보에 사용할 도서 ISBN 데이터 저장
-    root_win.text.configure(state=DISABLED)
-    if plus != 0:
-        root_win.rentB_label.config(text="대출도서:{} 외 {}권".format(win.chk_list[0][0], plus))
+    if len(win.chk_list)==0:    #선택된 도서가 없을 경우
+        messagebox.showwarning("도서 선택", "도서가 선택되지 않았습니다.", parent=win.newWindow)
     else:
-        root_win.rentB_label.config(text="대출도서:{}".format(win.chk_list[0][0]))
-    root_win.rent_day.config(text="대여일:{}".format(dt.datetime.now().date()))
-    root_win.re_day.config(text="반납예정일:{}".format(dt.datetime.now().date()+dt.timedelta(days=7)))
-    win.newWindow.withdraw()
+        root_win.text.config(state=NORMAL)  #텍스트 위젯을 비워주기 위해 위젯 상태를 변경가능한 상태로 설정
+        root_win.text.delete("1.0", "end")
+        plus = 0
+        for i in range(len(win.chk_list)):
+            for j in win.chk_list[i]:
+                root_win.text.insert('end', str(j)+' | ')
+            root_win.text.insert('end', '\n')
+            if i > 0:
+                plus += 1
+            rent.rent_book.append(win.chk_list[i][3])   #대출정보에 사용할 도서 ISBN 데이터 저장
+        root_win.text.configure(state=DISABLED)
+        if plus != 0:
+            root_win.rentB_label.config(text="대출도서:{} 외 {}권".format(win.chk_list[0][0], plus))
+        else:
+            root_win.rentB_label.config(text="대출도서:{}".format(win.chk_list[0][0]))
+        root_win.rent_day.config(text="대여일:{}".format(dt.datetime.now().date()))
+        root_win.re_day.config(text="반납예정일:{}".format(dt.datetime.now().date()+dt.timedelta(days=7)))
+        win.newWindow.withdraw()
+
+#취소버튼 클릭시 확인창 -> 해당 기능이 실행된 창을 부모창으로 여기게끔 수정
+def exit(showText, win=0):
+        ask = messagebox.askquestion(showText, "취소하시겠습니까?", parent=win)
+        if ask == 'yes':
+            if win != 0:        #win의 인자를 전달받았을 경우(win이 0이 아닐 경우)
+                win.withdraw()  #withdraw():Toplevel의 메소드. Toplevel()로 생성한 외부창을 화면에서 제거해준다.
+        else:
+            return
 
 #회원 선택창
 def User_Choice(window, root_win, rent, re=False):
     new_win = new_window()
     new_win.make_window(window, '회원 선택')
     #검색창 설정
-    new_win.Search_bar(13, lambda:search_info(new_win, book, user))
+    new_win.Search_bar(13, lambda:search_info(new_win, U_DataFrame=user))   #탈퇴회원을 제외한 데이터프레임 전달
     #확인/취소 버튼
-    new_win.under_button_R(lambda:chkU_def(new_win, root_win, rent, re))
+    new_win.under_button_R(lambda:chkU_def(new_win, root_win, rent, re), lambda:exit('회원 선택', new_win.newWindow))
     #회원 목록이 출력될 프레임
     user_list = list()      #user 출력형 리스트 생성
     #전체 user를 출력형 list에 append -> 탈퇴회원을 제외하고 출력하도록 변경할 것
@@ -232,17 +240,21 @@ def Book_Choice(window, root_win, rent):
     new_win = new_window()
     new_win.make_window(window, '도서 선택')
     #검색창 설정
-    new_win.Search_bar(13, lambda:search_info(new_win, book, user, False))
+    new_win.Search_bar(13, lambda:search_info(new_win, book, choice=False)) #대출 중인 도서를 제외한 데이터프레임 전달
     #확인/취소 버튼
-    new_win.under_button_R(lambda:chkB_def(new_win, root_win, rent))
+    new_win.under_button_R(lambda:chkB_def(new_win, root_win, rent), lambda:exit('도서 선택', new_win.newWindow))
     #도서 목록이 출력될 프레임
     book_list = list()      #book 출력형 리스트 생성
+    book = pd.DataFrame()   #대출 중인 도서를 제외하고 저장할 데이터프레임 생성->검색 시 대상이 될 데이터 프레임
     #전체 book를 출력형 list에 append -> 대출 중인 도서를 제외하고 출력하도록 변경할 것
     for i in rent.book.index:
         #대출 정보에 존재하는 도서인지 확인
         if not rent.rent['BOOK_ISBN'].isin([rent.book['BOOK_ISBN'][i]]).any():
+            #book = book.append(rent.book.loc[i:i])
+            book = pd.concat([book, rent.book.loc[i:i]])
             book_list.append([rent.book.loc[i,'BOOK_TITLE'],rent.book.loc[i,'BOOK_AUTHOR'],rent.book.loc[i,'BOOK_PUB'],rent.book.loc[i,'BOOK_ISBN']])
         #반납된 도서인지 확인
         elif not rent.rent[rent.rent['BOOK_ISBN']==rent.book['BOOK_ISBN'][i]]['RETURN_DATE'].isna().any():
+            book = pd.concat([book, rent.book.loc[i:i]])
             book_list.append([rent.book.loc[i,'BOOK_TITLE'],rent.book.loc[i,'BOOK_AUTHOR'],rent.book.loc[i,'BOOK_PUB'],rent.book.loc[i,'BOOK_ISBN']])
     new_win.list_print("\t제목\t저자\t출판사\tISBN\t대출여부", book_list)
