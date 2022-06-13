74a77302f291454922e207b117d87467c0763541
ChoiPython <wndud7159@gmail.com>
2022-06-13 22:10:16 +0900

    도서 조회 데이터프레임 출력 완

--- Book_class.py
+++ Book_class.py
@@ -5,7 +5,7 @@ from tkinter.filedialog import *
 import pandas as pd
 import numpy as np
 from UI_Class import*
-import unicodedata
+from unicodedata import*
 pd.options.display.max_columns = None
 
 # 데이터프레임 사용
@@ -26,36 +26,44 @@ class Book_DataFrame():
         # self.book.to_csv('BOOK.csv', encoding='utf-8')
         # self.book_data = pd.read_csv('BOOK.csv') # 데이터 프레임 불러오기
 
+    # entry : width + 5 / 메인 창 크기 : 1060x720 
     # 등록되어 있는 도서 출력
     def Book_list_all(self) :
-        intext = []
+        intext = np.empty((0, len(self.book_data['BOOK_ISBN'])))
         for i in range(len(self.book_data['BOOK_ISBN'])):
-            intext.append([set_str(self.book_data.iloc[i]['BOOK_TITLE']) + set_str(self.book_data.iloc[i]['BOOK_AUTHOR'], 24) + set_str(self.book_data.iloc[i]['BOOK_PUB'], 15) +  set_str(str(self.book_data.iloc[i]['BOOK_ISBN']), 13)])
-            # intext.append([self.book_data.iloc[i]])
+            intext = np.append(intext, [set_str(self.book_data.iloc[i]['BOOK_TITLE']) + set_str(self.book_data.iloc[i]['BOOK_AUTHOR']) + set_str(self.book_data.iloc[i]['BOOK_PUB'], 25) +  set_str(str(self.book_data.iloc[i]['BOOK_ISBN']), 13)])
         return intext
 
 
     # 도서 검색
     def Book_Search(self, keyword) :
-        intext = []
         out_data = self.book_data.loc[self.book_data['BOOK_TITLE'].str.contains(keyword) | self.book_data['BOOK_AUTHOR'].str.contains(keyword)]
+        intext = np.empty((0, len(out_data['BOOK_ISBN'])))
         if self.book_data['BOOK_TITLE'].str.contains(keyword).any() or self.book_data['BOOK_AUTHOR'].str.contains(keyword).any():
             for i in range(len(out_data['BOOK_ISBN'])):
-                intext.append([self.book_data.iloc[i]['BOOK_TITLE'] + '\t\t' + self.book_data.iloc[i]['BOOK_AUTHOR'] + '\t\t' + self.book_data.iloc[i]['BOOK_PUB'] + '\t\t' + str(self.book_data.iloc[i]['BOOK_ISBN'])])
+                intext = np.append(intext, [set_str(out_data.iloc[i]['BOOK_TITLE']) + set_str(out_data.iloc[i]['BOOK_AUTHOR']) + set_str(out_data.iloc[i]['BOOK_PUB'], 25) +  set_str(str(out_data.iloc[i]['BOOK_ISBN']), 13)])
+                # intext = np.append(intext, [set_str(self.book_data.iloc[i]['BOOK_TITLE']) + set_str(self.book_data.iloc[i]['BOOK_AUTHOR'], 24) + set_str(self.book_data.iloc[i]['BOOK_PUB'], 15) +  set_str(str(self.book_data.iloc[i]['BOOK_ISBN']), 13)])
+                # intext = np.append(intext, [set_str(self.book_data.iloc[i]['BOOK_TITLE']) + '\t\t' + set_str(self.book_data.iloc[i]['BOOK_AUTHOR']) + '\t\t' + set_str(self.book_data.iloc[i]['BOOK_PUB']) + '\t\t' + set_str(str(self.book_data.iloc[i]['BOOK_ISBN']))])
         return intext
 
     # 도서 등록
     def Book_in(self, in_data) :
-        # isbn=in_data[3] # title=in_data[0] # author=in_data[1] # pub=in_data[2] # price=in_data[4] # link=in_data[5] # description=in_data[6]
-        new_data = pd.DataFrame([{'BOOK_ISBN' : in_data[3], 'BOOK_TITLE' : in_data[0], 'BOOK_AUTHOR' : in_data[1], 'BOOK_PUB' : in_data[2], 'BOOK_PRICE' : in_data[4], 'BOOK_LINK' : in_data[5], 'BOOK_DESCRIPTION' : in_data[6]}])
+        isbn=in_data[3] 
+        title=in_data[0] 
+        author=in_data[1] 
+        pub=in_data[2] 
+        price=in_data[4] 
+        link=in_data[5] 
+        description=in_data[6]
+        new_data = pd.DataFrame([{'BOOK_ISBN' : [isbn], 'BOOK_TITLE' : [title], 'BOOK_AUTHOR' : [author], 'BOOK_PUB' : [pub], 'BOOK_PRICE' :[price], 'BOOK_LINK' : [link], 'BOOK_DESCRIPTION' : [description]}])
         
-        if in_data[3] in self.book_data['BOOK_ISBN'] : # 이미 중복확인을 했음에도 중복 ISBN으로 등록했을 경우
+        if isbn in self.book_data['BOOK_ISBN'] : # 이미 중복확인을 했음에도 중복 ISBN으로 등록했을 경우
             messagebox.showinfo('도서관리프로그램', '이미 등록되어있는 도서입니다.')
 
         else :
             check = messagebox.askquestion('도서관리프로그램', '도서를 등록하시겠습니까?')
 
-            if check =='yes':
+            if check:
                 messagebox.showinfo('도서관리프로그램', '도서가 등록되었습니다.')
                 self.book_data = pd.concat([self.book_data, new_data], ignore_index=True)
                 self.book_data.to_csv('BOOK.CSV', encoding='utf-8', index = False)
@@ -97,29 +105,55 @@ class Book_DataFrame():
             else:
                 messagebox.showinfo('도서관리프로그램', '도서 수정이 취소되었습니다.')
 
-
+    # 도서 삭제
     def Book_del(self, isbn) :
         for i in range(len(isbn)):
             if self.book_data['BOOK_ISBN'].isin([isbn[i]]).any():
-                self.book_data.drop(self.book_data.loc[self.book_data['BOOK_ISBN'].isin([isbn[i]])].index, inplace=True)
-                self.book_data.to_csv('BOOK_csv', encoding='utf-8', index=False)
+                # if rent_ing:
+                #     messagebox.showinfo('도서관리프로그램', '※대여중인 도서는 삭제할 수 없습니다.')
+                ask = messagebox.askquestion('도서관리프로그램', '도서를 삭제하시겠습니까?')
+                if ask:
+                    self.book_data.drop(self.book_data.loc[self.book_data['BOOK_ISBN'].isin([isbn[i]])].index, inplace=True)
+                    self.book_data.to_csv('BOOK_csv', encoding='utf-8', index=False)
+                    messagebox.showinfo('도서관리프로그램', '도서가 삭제되었습니다.')
+
+                else:
+                    messagebox.showinfo('도서관리프로그램', '취소되었습니다.')
+
+    # def readcsv(self):
+    #     self.book_data = pd.read_csv('BOOK_csv', encoding='utf-8')
+
+    # def tocsv(self):
+    #     self.book_data.to_csv('BOOK_csv', encoding='utf-8', index=False)
+
 
 # 한글, 영어 자리수 맞추기 함수
-def set_str(input_s="", max_size=22, fill_char='  '):
+def set_str(input_s="", max_size=40, fill_char=' '):
     l = 0
-    for c in input_s:
-        if unicodedata.east_asian_width(c) in ['F', 'W']:
-            l+=2
+    for chr in input_s:
+        if east_asian_width(chr) in ['W']:
+            l+=3
         else :
             l+=1
     return input_s+fill_char*(max_size-l)
-# print(set_str("abnacada"))
-# print(set_str('한글한글한글한글'))
 
 
 
-
-
+#ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ출력 테스트ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+# # 등록되어 있는 도서 출력
+# book_data=pd.DataFrame({'BOOK_ISBN':[9788970504773,9791163030911,9788966263301, 9791160507812], 
+#                     'BOOK_TITLE':['파이썬과 데이터 과학', '점프투파이썬', 'it 지식', '모두의 데이터분석'],
+#                     'BOOK_AUTHOR':['천인국, 박동규, 강영민', '박응용', '브라이언', '송석리, 이현아'], 
+#                     'BOOK_PUB':['생능출판사','이지스퍼블리싱','인사이트','길벗'], 
+#                     'BOOK_PRICE':[26000, 18800, 20000, 19000], 
+#                     'BOOK_LINK':['http://www.yes24.com','http://www.yes24.com','http://www.yes24.com','http://www.yes24.com'],
+#                     'BOOK_DESCRIPTION':['파이썬 교재','파이썬 교재','it 교재','파이선 데이터 분석']})
+# def Book_list_all() :
+#     intext = np.empty((0, len(book_data['BOOK_ISBN'])))
+#     for i in range(len(book_data['BOOK_ISBN'])):
+#         intext = np.append(intext, [set_str(book_data.iloc[i]['BOOK_TITLE']) + set_str(book_data.iloc[i]['BOOK_AUTHOR'], 24) + set_str(book_data.iloc[i]['BOOK_PUB'], 15) +  set_str(str(book_data.iloc[i]['BOOK_ISBN']), 13)])
+#     return intext
+# print(Book_list_all())
 
 
 
