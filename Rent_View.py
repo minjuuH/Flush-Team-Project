72ac6eb9be1698738dcef64a59e53fdebda8fa5b
칸 <hwangmj1124@gmail.com>
2022-06-18 22:51:20 +0900

    회원 선택 기능 수정&출력 포맷 지정
    회원 선택 시 해당 회원의 대출 가능일이 지난 상태인지 확인하는 과정 추가

--- Rent_View.py
+++ Rent_View.py
@@ -61,31 +61,6 @@ def main_menu(window:Tk, uc=None):
     User.place(x=360, y=265)
     Rent.place(x=610, y=265)
 
-#테스트를 위한 도서/회원 데이터 프레임------------------------------
-#실제 사용 X -> 삭제해야할 내용
-book = pd.DataFrame({'BOOK_ISBN':[9788970504773,9791163030911,9788966263301, 9791160507812], 
-                     'BOOK_TITLE':['파이썬과 데이터 과학', '점프투파이썬', 'it 지식', '모두의 데이터분석'],
-                     'BOOK_AUTHOR':['천인국, 박동규, 강영민', '박응용', '브라이언', '송석리, 이현아'], 
-                     'BOOK_PUB':['생능출판사','이지스퍼블리싱','인사이트','길벗'], 
-                     'BOOK_PRICE':[26000, 18800, 20000, 19000], 
-                     'BOOK_LINK':['http://www.yes24.com','http://www.yes24.com','http://www.yes24.com','http://www.yes24.com'],
-                     'BOOK_IMAGE':['py.png','jump.png','it.png','data.png'], 
-                     'BOOK_DESCRIPTION':['파이썬 교재','파이썬 교재','it 교재','파이선 데이터 분석']})
-# book = pd.DataFrame(columns=['BOOK_ISBN','BOOK_TITLE','BOOK_AUTHOR','BOOK_PUB','BOOK_PRICE','BOOK_LINK','BOOK_IMAGE','BOOK_DESCRIPTION'])
-
-user = pd.DataFrame(columns=['USER_NAME','USER_BIRTH','USER_PHONE','USER_SEX','USER_MAIL','USER_IMEGE',
-                             'USER_RENT_CNT', 'USER_QUIT_DATE', 'USER_RENT_ALW', 'USER_WARN'])
-add_user = pd.DataFrame({'USER_NAME': ['이순신', '홍길동', '신사임당', '유관순', '장영실'],
-                     'USER_BIRTH': [ "1999-01-01", "1973-03-21", "1988-08-15", "1920-02-02", "2000-12-25"],
-                     'USER_PHONE': ['010-3965-4122', '010-1274-6349', '010-2910-0058',
-                                   '010-9001-1004', '010-0308-1734'],
-                     'USER_SEX': [True, True, False, False, True],
-                     'USER_MAIL': ['lee0101@naver.com', 'hong0321@naver.com','sinsa0815@daum.net',
-                                   'you0202@naver.com', 'hang1225@google.com'],
-                     'USER_IMEGE': ['person1.png', 'person2.png','person3.png','person4.png','person5.png'],
-                     'USER_RENT_CNT': [0, 0, 0, 0, 0],
-                     'USER_WARN': [0, 0, 0, 0, 0]})
-user = pd.concat([user, add_user])
 
 #---------------------------------창 화면 설계---------------------------------
 #도서 대출 화면
@@ -160,29 +135,35 @@ def Return_Screen(window, uc=None):
     #반납/취소 버튼
     new_win.under_button('반납', new_win.Base, bt3_def=lambda:main_menu(window, new_win), bt2_def=lambda:re_acc(rent_info,window,new_win))
     #대여 목록 출력
-    new_win.rent_list()
+    new_win.rent_list("{:>40s}{:>20s}{:>20s}{:>20s}{:>20s}".format('제목','저자','출판사','ISBN','반납예정일'))
 
 #회원 선택 기능 -> 확인 버튼 클릭 시 수행
 def chkU_def(win, root_win, rent, re=False):
     if len(win.chk_list)==0:    #선택된 회원이 없을 경우
         messagebox.showwarning("회원 선택", "회원을 선택하십니오.", parent=win.newWindow)
     elif len(win.chk_list)==1:  #한 명의 회원만 선택되었을 경우 -> 정상 작동
-        root_win.userinfo.config(text=win.chk_list[0][0]+' | '+win.chk_list[0][1]+' | '+win.chk_list[0][2])
         if re:  #반납창에서 회원 선택을 수행할 경우
             rent_DF = rent.existence(win.chk_list[0][2])
             rent.re_user = win.chk_list[0][2]   #반납할 회원 정보 rent 객체에 저장
             book_list = list()
             for i in rent_DF['BOOK_ISBN']:
+                #편의성을 위해 대출 도서 정보에 반납예정일을 띄워줌
+                re_due_day = rent_DF.loc[rent_DF[rent_DF['BOOK_ISBN']==i].index[0], 'RETURN_DUE_DATE']
                 rent_book = rent.book[rent.book['BOOK_ISBN']==i]
                 idx = rent_book.index
-                book_list.append([rent_book.loc[idx[0],'BOOK_TITLE'],rent_book.loc[idx[0],'BOOK_AUTHOR'],rent_book.loc[idx[0],'BOOK_PUB'],rent_book.loc[idx[0],'BOOK_ISBN']])
+                book_list.append([rent_book.loc[idx[0],'BOOK_TITLE'],rent_book.loc[idx[0],'BOOK_AUTHOR'],rent_book.loc[idx[0],'BOOK_PUB'],rent_book.loc[idx[0],'BOOK_ISBN'],re_due_day])
             root_win.info_list(text_del=1, list=book_list)
-        else:   #대출창에서 회원 선택을 수행할 경우
+            root_win.userinfo.config(text=win.chk_list[0][0]+' | '+win.chk_list[0][1]+' | '+win.chk_list[0][2])
+            win.newWindow.withdraw()
+        elif rent.rent_allow(win.chk_list[0][2]):   #대출창에서 회원 선택을 수행할 경우
             root_win.rentU_label.config(text="대여인:"+win.chk_list[0][0])
             rent.rent_user = win.chk_list[0][2] #대출정보에 사용할 회원 전화번호 데이터 저장
             root_win.rent_day.config(text="대여일:{}".format(dt.datetime.now().date()))
             root_win.re_day.config(text="반납예정일:{}".format(dt.datetime.now().date()+dt.timedelta(days=7)))
-        win.newWindow.withdraw()
+            root_win.userinfo.config(text=win.chk_list[0][0]+' | '+win.chk_list[0][1]+' | '+win.chk_list[0][2])
+            win.newWindow.withdraw()
+        else:
+            messagebox.showinfo('회원 선택', '연체된 회원은 대출이 불가합니다.', parent=win.newWindow)
     else:                       #선택된 회원이 한 명을 초과한 경우
         messagebox.showwarning("회원 선택", "회원 선택은 1명만 가능합니다.", parent=win.newWindow)
         
@@ -233,7 +214,7 @@ def User_Choice(window, root_win, rent, re=False):
     user = rent.user[rent.user['USER_QUIT_DATE'].isna()]    #탈퇴회원을 제외한 회원 목록
     for i in user.index:
         user_list.append([user.loc[i,'USER_NAME'],user.loc[i,'USER_BIRTH'],user.loc[i,'USER_PHONE']])
-    new_win.list_print("\t이름\t\t생년월일\t전화번호", user_list)
+    new_win.list_print("{:>28s}{:>25s}{:>25s}".format('이름','생년월일','전화번호'), user_list)
 
 #도서 선택창
 def Book_Choice(window, root_win, rent):
@@ -257,4 +238,4 @@ def Book_Choice(window, root_win, rent):
         elif not rent.rent[rent.rent['BOOK_ISBN']==rent.book['BOOK_ISBN'][i]]['RETURN_DATE'].isna().any():
             book = pd.concat([book, rent.book.loc[i:i]])
             book_list.append([rent.book.loc[i,'BOOK_TITLE'],rent.book.loc[i,'BOOK_AUTHOR'],rent.book.loc[i,'BOOK_PUB'],rent.book.loc[i,'BOOK_ISBN']])
-    new_win.list_print("\t제목\t저자\t출판사\tISBN\t대출여부", book_list)
+    new_win.list_print("{:>28s}{:>25s}{:>25s}{:>25}".format('제목','저자','출판사','ISBN'), book_list)
